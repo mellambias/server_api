d3214e32f96c5e6f25a99ed8341fddc9c9ef4ae0
mellambias <mellambias@gmail.com>
2023-01-31 20:00:22 +0100

    menu

--- api/controllers/ControllerMenu.js
+++ api/controllers/ControllerMenu.js
@@ -8,21 +8,42 @@ class ControllerMenu extends Controller {
 
     async getMenuItems(menuName = {}) {
         try {
+            const result = [];
+            const pila = [];
+            const pilaName = [];
+            let current = {};
+            let temp = {};
+            let children = [];
             const root = await this.model.findOne({
                 where: { name: menuName },
             });
-            const children = await root.getChildren();
-            console.log(children);
-            const item = {
-                id: root.id,
-                name: root.name,
-                customUrl: root.customUrl,
-                order: root.order,
-                parentId: root.parentId,
-                children,
-            };
-
-            return item;
+            current = root;
+            do {
+                pila.push(current);
+                pilaName.push(current.name);
+                do {
+                    children = await current.getChildren();
+                    if (children.length) {
+                        children.forEach(element => {
+                            pila.push(element);
+                            pilaName.push(element.name);
+                        });
+                        result.push(current.name);
+                        current = pila.pop();
+                        pilaName.pop();
+                    } else {
+                        result.push(current.name);
+                        current = pila.pop();
+                        pilaName.pop();
+                    }
+                } while (current != undefined || pila.length > 0);
+                if (pila.length != 0) {
+                    temp = pila.pop();
+                    console.log(temp.dataValues);
+                    current = pila.pop();
+                }
+            } while (current != undefined || pila.length > 1); //Si `actual` es distinto de NULL o la pila no está vacía, ir al paso 3
+            return result;
         } catch (err) {
             console.log(err);
         }
